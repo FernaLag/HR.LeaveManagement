e2587e4387169c147a81234a6d053df1479e1786
Ferna Lag <smilefer_@hotmail.com>
2022-07-30 11:01:41 -0300

    Add project files.

---
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+        Task<LeaveAllocation> GetLeaveRequestWithDetails(int id);
+        Task<List<LeaveAllocation>> GetLeaveRequestsWithDetails();
+    }
+}
