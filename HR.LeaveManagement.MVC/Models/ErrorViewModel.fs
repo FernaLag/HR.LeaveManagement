8e0188473614da87bd6c41495270e5ac6327255f
Ferna Lag <smilefer_@hotmail.com>
2022-08-07 10:25:46 -0300

    Adding Libs, references, Interfaces and Classes

--- HR.LeaveManagement.MVC/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace HR.LeaveManagement.MVC.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
